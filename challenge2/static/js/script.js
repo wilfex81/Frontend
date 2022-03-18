20221c92e4787721d0a9bc0d5a10983b2d673845
wilfex <wilfex81@gmail.com>
2022-03-18 12:32:33 +0300

    challenge 2

--- challenge2/static/js/script.js
+++ challenge2/static/js/script.js
@@ -1,5 +1,5 @@
 alert('Click me severally(Generate cat) and i will make your day')
-function generateCut(){
+function generateCat(){
     let image = document.createElement('img');
     let div = document.getElementById('flex-cat-gen');
     image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
